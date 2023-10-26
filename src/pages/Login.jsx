c268964e8937e6f6e21b738fa264c12b0575f796
jignesh sharma <jigneshsharma9868@gmail.com>
2023-10-26 13:13:20 +0530

    design of both register and login page is done

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,21 @@
+import { Link } from "react-router-dom";
 
 
 const Login = () => {
     return(
         <>
-         Login page
+        <div className="w-full flex justify-center items-center h-[80vh]">
+            <div className="flex flex-col justify-center items-center space-y-4 w-[80%] md:w-[25%]">
+                <h1 className="text-xl font-bold text-left">Login To Your Account</h1>
+                <input type="text" className="w-full px-4 py-2 border-2 border-black outline-0" placeholder="Enter Your Email" />
+                <input type="password" className="w-full px-4 py-2 border-2 border-black outline-0" placeholder="Enter Your Password" />
+                <button className="w-full px-4 py-4 text-lg font-bold text-white bg-black hover:bg-gray-500 hover:text-black">Login</button>
+             <div className="flex justify-center items-center space-x-2 ">
+                <p>New Here?</p>
+                <p className="text-gray-500 hover:text-black"><Link to="/register">Register</Link></p>
+             </div>
+            </div>
+        </div>
         </>
     )
 }
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,20 @@
-
+import {Link} from 'react-router-dom'
 const Register = () => {
     return(
-
         <>
-         Register page
+        <div className="w-full flex justify-center items-center h-[80vh]">
+            <div className="flex flex-col justify-center items-center space-y-4 w-[80%] md:w-[25%]">
+                <h1 className="text-xl font-bold text-left">Login To Your Account</h1>
+                <input type="text" className="w-full px-4 py-2 border-2 border-black outline-0" placeholder="Enter Your Name" />
+                <input type="text" className="w-full px-4 py-2 border-2 border-black outline-0" placeholder="Enter Your Email" />
+                <input type="password" className="w-full px-4 py-2 border-2 border-black outline-0" placeholder="Enter Your Password" />
+                <button className="w-full px-4 py-4 text-lg font-bold text-white bg-black hover:bg-gray-500 hover:text-black">Register</button>
+             <div className="flex justify-center items-center space-x-2 ">
+                <p>Already Have an Account?</p>
+                <p className="text-gray-500 hover:text-black"><Link to="/login">Login</Link></p>
+             </div>
+            </div>
+        </div>
         </>
     )
 }
