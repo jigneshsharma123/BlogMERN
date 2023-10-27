b0b3a4aeb0af69628b4c3873ecbafe2dbf652026
jignesh sharma <jigneshsharma9868@gmail.com>
2023-10-27 06:26:53 +0530

    frontend completed not its' time to move with backend

--- src/pages/PostDetails.jsx
+++ src/pages/PostDetails.jsx
@@ -1,3 +1,4 @@
+import Comment from "../components/Comment";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import {BiEdit} from "react-icons/bi";
@@ -38,37 +39,10 @@ const PostDetails = () => {
              {/* comment */}
             <div className="flex flex-col mt-4">
                 <h3 className="mt-6 mb-4 font-semibold">Comments:</h3>
-               
-              <div className="px-2 py-2 bg-gray-200 rounded-lg my-4">
-                <div className="flex items-center justify-between">
-                    <h3 className="font-bold text-gray-600">@jignesh</h3>
-                    <div className="flex justify-center items-center space-x-4">
-                    <p className="text-gray-500 text-sm">20/02/2023</p>
-                    <p className="text-gray-500 text-sm">16:45</p>
-                    <div className="flex justify-center items-center space-x-2">
-                        <p className=""><BiEdit/></p>
-                        <p className=""><MdDelete /></p>
-                    </div>
-                    </div>
-                </div>
-                <p className="px-4 mt-2">Nice information</p>
-
-              </div>
-              <div className="px-2 py-2 bg-gray-200 rounded-lg my-4">
-                <div className="flex items-center justify-between">
-                    <h3 className="font-bold text-gray-600">@jignesh</h3>
-                    <div className="flex justify-center items-center space-x-4">
-                    <p className="text-gray-500 text-sm">20/02/2023</p>
-                    <p className="text-gray-500 text-sm">16:45</p>
-                    <div className="flex justify-center items-center space-x-2">
-                        <p className=""><BiEdit/></p>
-                        <p className=""><MdDelete /></p>
-                    </div>
-                    </div>
-                </div>
-                <p className="px-4 mt-2">Nice information</p>
-
-              </div>
+               <Comment/>
+               <Comment/>
+               <Comment/>
+            
             </div> {/*end of the comment section */}
 
          
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,5 @@
 import {Link} from 'react-router-dom'
+import Footer from '../components/Footer';
 const Register = () => {
     return(
         <>
@@ -19,6 +20,7 @@ const Register = () => {
              </div>
             </div>
         </div>
+        <Footer />
         </>
     )
 }
